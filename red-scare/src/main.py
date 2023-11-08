5d50e5c5b07601435687f3c9778a4350c4938ad3
Magnus Pedersen <magnus4p@gmail.com>
2023-11-08 15:50:06 +0100

    work work work
    Co-authored-by: Lukas Vinther Offenberg <LVOL98@users.noreply.github.com>
    Co-authored-by: nickarlsen <nickarlsen@users.noreply.github.com>
    Co-authored-by: Malou <MalouLandsgaard@users.noreply.github.com>

--- red-scare/src/main.py
+++ red-scare/src/main.py
@@ -1,4 +1,7 @@
+import sys
 import networkx as nx
+
+sys.setrecursionlimit(100000)
 
 # import data
 n, m, r = map(int, input().split())
@@ -11,7 +14,7 @@ print("start, end -> ", start, end)
 # Docs for our Graph Type -> https://networkx.org/documentation/stable/tutorial.html
 G = nx.Graph()
 for i in range(n):
-    word = input()
+    word = input().strip()
     if word.endswith('*'):
         wordNoRed = word.split('*')[0].strip()
         G.add_node(wordNoRed, red=True, weight=1)
@@ -20,7 +23,9 @@ for i in range(n):
 
 for i in range(m):
     word1, word2 = input().split(' -- ')
-    G.add_edge(word1, word2)
+    G.add_edge(word1.strip(), word2.strip())
+
+redList = list(filter(lambda x: x[1], G.nodes(data='red')))
 
 
 
@@ -56,18 +61,62 @@ def none():
         if GnoReds.nodes[node]['red']:
             GnoReds.remove_node(node)
     # Do Astar Path
-    path = findPathAstar(GnoReds, start, end)
+    try:
+        path = findPathAstar(GnoReds, start, end)
+    except Exception: 
+        path = None
     # Return result for none()
     return -1 if path == None else len(path)-1 # -1 because we want the number of edges, not nodes
 
 # Some - Does there exist a path that uses at least one Red Vertex?
 # return 'true' if such a path exists, else return 'false'
 def some():
-    return "not implemented"
+    Gcopy = G.copy()
+    for (red, _) in redList:
+        try:
+            findPathAstar(Gcopy, start, red)
+            findPathAstar(Gcopy, red, end)
+            return True
+        except Exception:
+            continue
+    # This is assuming we didnt find a valid path from Start to Red to End
+    return False
 
 # Many - Return the maximum number of Red Vertices that can be used in a path
 # return number of red vertices if possible (can be 0), else if no path exist return -1
 def many():
+    Gcopy = G.copy()
+
+    # Check if any path exist at all
+    try: 
+        findPathAstar(Gcopy, start, end)
+    except Exception: 
+        return -1
+
+    # Find valid paths from start to red, and red to end 
+    from_start_to_red_list = []
+    from_red_to_end_list = []
+    print("redlist", redList)
+    for red in redList:
+        print("start, red", start, red)
+        try:
+            # TODO I have no idea why it cannot find a path, there should be one
+            path = findPathAstar(Gcopy, start, red)
+            print("path", path)
+            from_start_to_red_list.append()
+        except Exception:
+            dummy = 1
+        try:
+            from_red_to_end_list.append(findPathAstar(Gcopy, red, end))
+        except Exception: 
+            dummy = 1
+
+    temp_best_result = 0
+    
+    print("from_start", from_start_to_red_list)
+    print("from_red", from_red_to_end_list)
+    
+
     return "not implemented"
 
 # Few - Return the minimum number of Red Vertices that must be used in a path
@@ -83,6 +132,7 @@ def alternate():
 ### Format Output
 print("Result for None:", none())
 print("Result for Some:", some())
+# If some returns false, there are 0 in many
 print("Result for Many:", many())
 print("Result for Few:", few())
 print("Result for Alternate:", alternate())
