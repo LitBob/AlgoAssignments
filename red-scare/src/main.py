eb68fed01702f971cd86f85d2e234aa9f1c49a2f
Magnus Pedersen <magnus4p@gmail.com>
2023-11-22 15:47:46 +0100

    Only missing "Few"
    Co-authored-by: Lukas Vinther Offenberg <LVOL98@users.noreply.github.com>
    Co-authored-by: nickarlsen <nickarlsen@users.noreply.github.com>
    Co-authored-by: Malou <MalouLandsgaard@users.noreply.github.com>

--- red-scare/src/main.py
+++ red-scare/src/main.py
@@ -1,13 +1,13 @@
 import sys
 import networkx as nx
 
-sys.setrecursionlimit(100000)
+sys.setrecursionlimit(1000000)
 
 # import data
 n, m, r = map(int, input().split())
 start, end = input().split()
 
-graphIsDirected = False
+graphIsDirected = True
 
 print("n, m, r -> ", n, m, r)
 print("start, end -> ", start, end)
@@ -27,7 +27,7 @@ for i in range(m):
     edge_input = input()
 
     if ' -- ' in edge_input:
-        graphIsDirected = True
+        graphIsDirected = False
         word1, word2 = edge_input.split(' -- ')
         # word1_out = f"{word1}_out"
         # G.add_node()
@@ -105,9 +105,11 @@ def none():
 ## Some - Does there exist a path that uses at least one Red Vertex?
 ## return 'true' if such a path exists, else return 'false'
 def some():
-
-    # TODO : We made splitVertices() last time and need to use it there for UNDIRECTED GRAPHS
     GFlow = G.copy()
+    # TODO : We made split_vertices() but maybe should test more throughly
+    if not graphIsDirected:
+        GFlow = split_vertices()
+
     # Add new source (called start from now) vertice 
     GFlow.add_node("source")
     # Add a new sink vertice with an edge from both source and sink to it
@@ -147,52 +149,92 @@ def some():
 
 # Many - Return the maximum number of Red Vertices that can be used in a path
 # return number of red vertices if possible (can be 0), else if no path exist return -1
+
+## CHAT ZONE
+
+# Assume the graph is represented as an adjacency list where graph[node] contains neighbors of 'node'
+# 'is_red(node)' returns True if the node is red, otherwise False
+
+def max_red_nodes(graph, node, end_node, memo):
+    # Base case: If the current node is the end node, return 1 if it's red, else return 0
+    if node == end_node:
+        return 1 if graph.nodes[node]['red'] else 0
+
+    # Check if the result for the current node has already been computed
+    if node in memo:
+        return memo[node]
+
+    max_red_count = 0
+    # Traverse through neighbors of the current node
+    for neighbor in graph[node]:
+        # Recursively find the maximum count of red nodes for each neighbor
+        max_red_count = max(max_red_count, max_red_nodes(graph, neighbor, end_node, memo))
+
+    # Add 1 to the count if the current node is red
+    if graph.nodes[node]['red']:
+        max_red_count += 1
+
+    # Memoize the result for the current node
+    memo[node] = max_red_count
+    return max_red_count
+
 def many():
     # Can be sovled if graph is directed and acyclic
     # use dynamic programming for this 
     Gcopy = G.copy()
 
-    # Check if any path exist at all
-    try: 
-        findPath(Gcopy, start, end)
-    except Exception: 
+    # { from - to: seen before? }
+    memoization = {}
+
+    # Check if graph is directed and acyclic
+    if graphIsDirected and nx.is_directed_acyclic_graph(Gcopy):
+        # This means that it can be solved with dynamic programming
+
+        # But we just quickly check if there even is a path
+        try: 
+            findPath(Gcopy, start, end)
+        except:
+            return -1
+        
+        return max_red_nodes(Gcopy, start, end, memoization)
+
+    else:
         return -1
-
-    # Find valid paths from start to red, and red to end 
-    from_start_to_red_list = []
-    from_red_to_end_list = []
-    print("redlist", redList)
-    for red in redList:
-        print("start, red", start, red)
-        try:
-            # TODO I have no idea why it cannot find a path, there should be one
-            path = findPath(Gcopy, start, red)
-            print("path", path)
-            from_start_to_red_list.append()
-        except Exception:
-            dummy = 1
-        try:
-            from_red_to_end_list.append(findPath(Gcopy, red, end))
-        except Exception: 
-            dummy = 1
-
-    temp_best_result = 0
-    
-    print("from_start", from_start_to_red_list)
-    print("from_red", from_red_to_end_list)
-    
-
-    return "not implemented"
 
 # Few - Return the minimum number of Red Vertices that must be used in a path
 # return the number of red vertices if possible, else if no path exists return -1
 def few():
-    return "not implemented"
+    
+    return "not implemented - NP Hard???"
 
 # Alternate - Does there exist a path that alternates between Red and Non-Red Vertices?
 # return 'true' if possible, otherwise return 'false' 
 def alternate():
-    return "not implemented"
+    visited = []
+    
+    def recurse(graph, node, end, visited):
+        visited.append(node)
+        
+        currentColor = 'red' if graph.nodes[node]['red'] else 'white'
+
+        if node == end:
+            return True
+
+        currentAnswer = False
+        for toNode in graph[node]:
+            nextColor = 'red' if graph.nodes[toNode]['red'] else 'white'
+
+            if currentColor == nextColor:
+                continue
+
+            if toNode in visited:
+                continue
+
+            currentAnswer = currentAnswer or recurse(graph, toNode, end, visited)
+
+        return currentAnswer
+
+    return recurse(G, start, end, visited)
 
 ### Format Output
 print("Result for None:", none())
